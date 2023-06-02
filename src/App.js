297cde88d4efb8ca4e4af601773038cb4baaf1fb
SantaKlaussss <dsfdsfvdsvf@mail.ru>
2023-06-02 21:28:24 +0300

    change description price

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { store } from "./redux";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/home-page/HomePage";
+import Header from "./components/header/Header";
+import GamePage from "./Pages/game-page/game-page";
+import OrderPage from "./Pages/order-page/OrderPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router >
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+          <Routes>
+            <Route path="/app/:title" element={<GamePage />} />
+          </Routes>
+          <Routes>
+            <Route path="/order" element={<OrderPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
