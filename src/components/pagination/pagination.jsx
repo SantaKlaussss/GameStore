6a85cf367027654dfcb04df2d8cce355e3dc4bfd
SantaKlaussss <dsfdsfvdsvf@mail.ru>
2023-06-04 17:08:33 +0300

    pagination activ

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -4,10 +4,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setCurrentPage } from '../../redux/games/reducer';
 
 const Pagination = () => {
-  debugger
+
+  const [isActive, setIsActive] = useState(false)
+
   const dispatch = useDispatch()
   const onChangedPage = (p) => {
     dispatch(setCurrentPage(p))
+    setIsActive(true)
   }
 
   const data = useSelector(store => store.games);
@@ -15,7 +18,7 @@ const Pagination = () => {
   const { pageSize, currentPage, totalGameCount, portionSize } = data;
 
   let pageCount = Math.ceil(totalGameCount / pageSize);
-
+  
   let pages = [];
 
   for (let i = 1; i <= pageCount; i++ ) {
@@ -24,7 +27,8 @@ const Pagination = () => {
   
   return (
     <div>
-      {pages.map(p => <span className='block-pagination' key={p} onClick={() => onChangedPage(p)}>{p}</span>)}
+      {pages.map(p => <span className={(currentPage == p) ? 'block-pagination block-pagination__active' : 'block-pagination'} 
+      key={p} onClick={() => onChangedPage(p)}>{p}</span>)}
     </div>
   )
 }
